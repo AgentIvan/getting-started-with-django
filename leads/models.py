853ad2193f4b54b786e5ce7dff12fe42f8241d48
Ivan D. WLS <I.Derzhylo@softpositive.com>
2021-12-20 00:07:15 -0600

    use AbstractUser model

--- leads/models.py
+++ leads/models.py
@@ -1,18 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+
+class Agent(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Lead(models.Model):
-    SOURCE_CHOISES = (
-        ("YouTube", "YouTube"),
-        ("Google", "Google"),
-        ("Newsletter", "Newsletter"),
-    )
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(choices=SOURCE_CHOISES, max_length=100)
-
-    profile_picture = models.ImageField(blank=True, null=True)
-    special_files = models.FileField(blank=True, null=True)
+    agent = models.ForeignKey(Agent, on_delete=models.deletion.CASCADE)
