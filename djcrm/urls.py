8bf53e58d1f7ea808460bb4b551602579b661742
Ivan D. WLS <I.Derzhylo@softpositive.com>
2021-12-22 11:01:45 -0600

    Hello world view

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from leads.views import home_page
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", home_page)]
