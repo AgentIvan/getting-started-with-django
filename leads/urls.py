816212b2c1bced502ce385b90325ebbebd413686
Ivan D. WLS <I.Derzhylo@softpositive.com>
2021-12-22 20:46:26 -0600

    Form and lead create view

--- leads/urls.py
+++ leads/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import lead_list, lead_detail
+from .views import lead_create, lead_detail, lead_list
+
 
 app_name = "leads"
 
-urlpatterns = [path("", lead_list), path("<pk>/", lead_detail)]
+urlpatterns = [
+    path("", lead_list),
+    path("<int:pk>/", lead_detail),
+    path("create/", lead_create),
+]
