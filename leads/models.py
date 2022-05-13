eb568322ffdd6e4b62c3e256151e729ed3b9a27f
Ivan Derzhylo <i.derzhylo@softpositive.com>
2022-05-13 23:50:37 +0300

    Category model

--- leads/models.py
+++ leads/models.py
@@ -15,14 +15,6 @@ class UserProfile(models.Model):
         return self.user.username + " profile"
 
 
-def post_user_created_signal(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-
-post_save.connect(receiver=post_user_created_signal, sender=User)
-
-
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
@@ -31,12 +23,28 @@ class Agent(models.Model):
         return "Agent's email: " + self.user.email
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)  # New, Contacted, Converted, Unconverted
+
+    def __str__(self):
+        return f"{self.pk}. {self.name}"
+
+
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey(Agent, null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(receiver=post_user_created_signal, sender=User)
